132ef4ef98730466bde9443cec4fad4a5d940fff
Arpan Maity <136701938+Arpangeek@users.noreply.github.com>
2024-06-07 21:39:10 +0530

    first commit

---
+++ Projects/Myntra-Clone/3-myntra-react-clone/src/components/Login.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <>
+      <div
+        class="visme_d"
+        data-title="Untitled Project"
+        data-url="mxg7dk6d-untitled-project"
+        data-domain="forms"
+        data-full-page="false"
+        data-min-height="500px"
+        data-form-id="23086"
+      ></div>
+      <script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
+    </>
+  );
+};
+
+export default Login;
